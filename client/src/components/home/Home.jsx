762da377fdae3a68ffede81a05ce89ea80a02f03
mealboxhyd <MealBox987@gmail.com>
2024-08-11 16:12:30 +0530

    handling laoding state

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -24,71 +24,46 @@ function Home() {
       <CarouselLoader />
       <CustomLoader show={adminState.isLoading} />
 
-      {adminState.isLoading ? (
-        <Grid
-          container
-          spacing={3}
-          style={{
-            paddingLeft: "1rem",
-            marginTop: "0.5rem",
-            paddingRight: "1rem",
-          }}
-        >
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item) => {
-            return (
-              <Grid item xs={12} md={4} lg={2}>
-                <Skeleton
-                  animation="wave"
-                  height={200}
-                  width="90%"
-                  style={{ marginBottom: 6 }}
-                />
-              </Grid>
-            );
-          })}
-        </Grid>
-      ) : (
-        categories.map((category) => {
-          return (
-            <Grid
-              container
-              spacing={3}
-              key={category._id}
-              style={{
-                paddingLeft: "1rem",
-                marginTop: "0.5rem",
-                paddingRight: "1rem",
-              }}
-            >
-              <Grid item xs={12}>
-                <Chip
-                  label={`Best Of ${category.name}`}
-                  color="primary"
-                  style={{ fontSize: "1rem", fontWeight: 600 }}
-                />
-              </Grid>
-
-              {/** Find category children id where it matches with product parent id */}
-              {products
-                .filter(
-                  (product) =>
-                    product.category.parentId ===
-                    category.children?.find(
-                      (cat) => cat._id === product.category.parentId
-                    )?._id
-                )
-                .slice(0, 6)
-                .map((product) => {
-                  return (
-                    <Grid item xs={6} md={4} lg={2}>
-                      <ProductCard product={product} />
-                    </Grid>
-                  );
-                })}
+      {categories.map((category) => {
+        return (
+          <Grid
+            container
+            spacing={3}
+            key={category._id}
+            style={{
+              paddingLeft: "1rem",
+              marginTop: "0.5rem",
+              paddingRight: "1rem",
+            }}
+          >
+            <Grid item xs={12}>
+              <Chip
+                label={`Best Of ${category.name}`}
+                color="primary"
+                style={{ fontSize: "1rem", fontWeight: 600 }}
+              />
             </Grid>
-          );
-        })
-      )}
+
+            {/** Find category children id where it matches with product parent id */}
+            {products
+              .filter(
+                (product) =>
+                  product.category.parentId ===
+                  category.children?.find(
+                    (cat) => cat._id === product.category.parentId
+                  )?._id
+              )
+              .slice(0, 6)
+              .map((product) => {
+                return (
+                  <Grid item xs={6} md={4} lg={2}>
+                    <ProductCard product={product} />
+                  </Grid>
+                );
+              })}
+          </Grid>
+        );
+      })}
     </div>
   );
 }
